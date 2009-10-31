3dac7acc35e3c2d8aa607e00f64d29c7e09883bc
paviad <none@none.com>
2009-10-31 13:29:59 +0000

    Forgot this file.

---
+++ GameInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Go
+{
+    public class GameInfo
+    {
+        public int Handicap { get; set; }
+        public double Komi { get; set; }
+
+        public GameInfo()
+        {
+            Komi = 0.5;
+        }
+    }
+}
